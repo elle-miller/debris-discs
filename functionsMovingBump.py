91dfda617a82d8d2c2dd70d5f87d56e3443ebb0d
Elle <elle.miller101@gmail.com>
2020-10-25 22:36:40 +1100

    Updated code for dustpy v2. Significant structure change, not compiling yet (stuck around planetesimal formation). Requires much testing

---
+++ functionsMovingBump.py
@@ -0,0 +1,137 @@
+import numpy as np
+from numpy import log, exp
+from dustpy.std.gas import Hp
+
+
+def initialGas(s, IniBumpPeakPos, A, width):
+    """
+    Initial gas profile at t = 0. Must call after first initialize so all parameters are set
+
+    Input:
+    ---------
+    IniBumpPeakPos: float
+    position of bump peak
+    A: float
+    amplitude
+    width: float
+    bump width scale factor
+    """
+    r = s.grid.r
+    sig_g = (r / s.ini.gas.SigmaRc) ** -s.ini.gas.SigmaExp * np.exp(-(r / s.ini.gas.SigmaRc) ** (2-s.ini.gas.SigmaExp))
+    M_gas = s.ini.gas.Mdisk * s.ini.star.M / (s.ini.dust.d2gRatio + 1.)  # total gas mass for given d2g
+    s.ini.gas.Sigma0 = M_gas / np.trapz(2 * np.pi * r * sig_g, x=r)
+    SigmaGas = (r / s.ini.gas.SigmaR0) ** -s.ini.gas.SigmaExp * np.exp(-(r/s.ini.gas.SigmaR0) ** (2-s.ini.gas.SigmaExp))
+    BumpPeakPos = getPeakPosition(s, IniBumpPeakPos=IniBumpPeakPos, TimeBumpForm=0, BumpVelFactor=0)
+    iniGas = Gauss(s, r, BumpPeakPos, A, width) * s.ini.gas.Sigma0 * SigmaGas
+    return iniGas
+
+
+def Gauss(s, r, BumpPeakPos, A, width):
+    """
+    Gaussian bump.
+
+    Input:
+    ---------
+    A: float
+    amplitude
+    r: array
+    radial grid
+    rpeak: float
+    peak position of bump
+    width: float
+    bump width scale factor
+    """
+    # Make sure bump at center of radial bin
+    iBumpPeakPos = (np.abs(r - BumpPeakPos)).argmin()
+    rpeak = r[iBumpPeakPos]
+    w_gap = width * Hp(s)  # Gas pressure scale height
+    return exp(-log(A) * exp(-0.5 * ((r - rpeak) / w_gap) ** 2))
+
+
+def BumpRadVel(s, iBumpPeakPos, BumpVelFactor):
+    """
+    Radial velocity of a bump according to Armitage (2010, Eq. (7.32)).
+    Input:
+    --------
+    iBumpPeakPos: integer
+        bin closest to current gas bump peak position
+    """
+    return -1.5 * BumpVelFactor * s.ini.gas.alpha * (s.gas.Hp[iBumpPeakPos] / s.grid.r[iBumpPeakPos]) ** 2 * \
+        s.grid.OmegaK[iBumpPeakPos] * s.grid.r[iBumpPeakPos]
+
+
+def getPeakPosition(s, IniBumpPeakPos, TimeBumpForm, BumpVelFactor):
+    """
+    Calculate the peak postion of the bump.
+
+    Input:
+    --------
+    IniBumpPeakPos: float
+        initial peak postion of bump
+    vRadBump: float
+        radial velocity of the moving gas bump at the peak position
+    TimeBumpForm: float
+        time at which a gas bump is forced
+    """
+    # Define and save the peak position
+    if s.t <= TimeBumpForm:
+        s.gas.BumpPeakPos = IniBumpPeakPos
+    elif s.t > TimeBumpForm:
+        iBumpPeakPos = (np.abs(s.grid.r - s.gas.BumpPeakPos)).argmin()
+        s.gas.BumpPeakPos += BumpRadVel(s, iBumpPeakPos=iBumpPeakPos, BumpVelFactor=BumpVelFactor) * s.dt
+    return s.gas.BumpPeakPos
+
+
+def renormalizeGasProfile(s, M_gas, IniBumpPeakPos, BumpVelFactor, A, width, TimeBumpForm):
+    """
+    For a non-evolving disk, the gas density is normalized in every timestep to guarantee mass conservation.
+    Input:
+    --------
+    M_gas: float
+        total gas disk mass [g]
+    width: float
+    bump width scale factor
+    """
+
+    if s.t >= TimeBumpForm:
+        r = s.grid.r
+        s.gas.Sigma = (r / s.ini.gas.SigmaR0) ** -s.ini.gas.SigmaExp * np.exp(
+            -(r / s.ini.gas.SigmaR0) ** (2 - s.ini.gas.SigmaExp))
+        BumpPeakPos = getPeakPosition(s, IniBumpPeakPos, TimeBumpForm, BumpVelFactor)
+        s.gas.Sigma += Gauss(s, r, BumpPeakPos, A, width)
+        SigmaNormConst = M_gas / np.trapz(2 * np.pi * s.grid.r * s.gas.Sigma, x=s.grid.r)
+        s.gas.Sigma = SigmaNormConst * s.gas.Sigma
+    return s.gas.Sigma
+
+
+def alphaBumps(s, IniBumpPeakPos, A, width, TimeBumpForm, BumpCreatedViaAlpha, BumpVelFactor):
+    """
+    Set turbulence values. This function is called in the code and
+    will be used to call the other funtions that change the gas density.
+
+    Input:
+    --------
+    IniBumpPeakPos: float
+    initial position of bump
+    A: float
+    amplitude of bump
+    TimeBumpForm: float
+    time after which a bump is allowed to form
+    width: float
+    bump width scale factor
+    """
+    r = s.grid.r
+    bumpyAlpha = s.ini.gas.alpha * np.ones_like(r)
+    M_gas = s.ini.gas.Mdisk * s.ini.star.M / (s.ini.dust.d2gRatio + 1.)  # total gas mass for given d2g
+
+    if not BumpCreatedViaAlpha:
+        s.gas.Sigma = renormalizeGasProfile(s, M_gas, IniBumpPeakPos, BumpVelFactor, A, width, TimeBumpForm)
+    else:
+        # If the peak position of the bump goes beyond the set minimum radius, exit the program
+        BumpPeakPos = getPeakPosition(s, IniBumpPeakPos, TimeBumpForm, BumpVelFactor)
+        if BumpPeakPos < s.ini.grid.rmin:
+            print("Exiting")
+            exit()
+        else:
+            bumpyAlpha = s.ini.gas.alpha / Gauss(s, r, BumpPeakPos, A, width)
+    return bumpyAlpha
---
+++ main.py
@@ -0,0 +1,219 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+###############################################################################
+# main.py
+# Main file for program to evolve a protoplanetary disc under initial conditions
+# To run:
+# python main.py -flag1 val1 -flag2 val2 etc.
+
+from dustpy.simulation import Simulation
+from dustpy import constants as c
+from functionsMovingBump import alphaBumps, initialGas
+from functionsPlanFormation import setPlanetesimalFormation, dustSources, addPlanetesimals
+import numpy as np
+import argparse
+from os import path
+
+
+##################### MAIN FUNCTION ###########################################
+def main(args):
+    # Create instance
+    s = Simulation()
+
+    # Print initial conditions of input args
+    setInitConds(s, args, verbose=True)
+
+    # Bind systoles and diastoles for planetesimal formation
+    setPlanForm(s)
+
+    # Bind alpha bump function to create gas gap
+    s.gas.alpha.update(alphaBumps(s, args.iniBumpPeakPos * c.au, args.amplitude, args.width, args.timeBumpForm,
+                                  args.gasEvolution, args.bumpVelFactor))
+
+    # Initialise once to get pressure scale height
+    s.initialize()
+
+    # Bind initial gas profile
+    s.gas.update(initialGas(s, args.iniBumpPeakPos * c.au, args.amplitude, args.width))
+    s.ini.dust.allowDriftLimitedParticles = True
+    s.initialize()
+
+    # Specify where to put the data and simulation related info
+    setSimulationParams(s, args)
+
+    # Run the simulation
+    print("Evolving...")
+    s.run()
+
+
+##################### OTHER FUNCTIONS #########################################
+
+def setInitConds(s, args, verbose):
+    """
+    Set initial conditions of simulation model
+
+    Input:
+    ---------
+    s: Instance of the Simulation class
+    args: command line arguments
+    verbose: bool
+    """
+    # Radial grid
+    s.ini.grid.rmin = c.au * args.rmin
+    s.ini.grid.rmax = c.au * args.rmax
+    s.ini.grid.Nr = args.Nr
+    s.makegrids()
+
+    # Mass grid
+    s.ini.grid.Nm = args.Nm
+    s.ini.grid.mmax = args.massMax  # default is 1e5
+    if args.dustEvolution:
+
+        # Only enters for alpha0=1e-4
+        if args.alpha < 3e-4:
+            s.ini.grid.mmax = 1e14
+        else:
+            # This is where alpha0=1e-3 goes
+            s.ini.grid.mmax = 1e8
+    else:
+        s.ini.grid.mmax = 1e1
+
+    # Gas
+    s.ini.gas.Mdisk = args.MdiskInMstar * c.M_sun
+    s.ini.gas.alpha = args.alpha * np.ones_like(s.grid.r)
+
+    # Dust
+    s.ini.dust.aIniMax = args.aIniMax
+    s.ini.dust.deltaRad = s.ini.gas.alpha  # radial particle diffusion
+    s.ini.dust.deltaTurb = s.ini.gas.alpha  # relative velocitiy turbulence
+    s.ini.dust.deltaVert = s.ini.gas.alpha  # vertical diffusion
+
+    # Star
+    s.ini.star.M = c.M_sun * args.starmass
+
+    s.initialize()
+
+    if verbose:
+        print("minyear = %d" % args.minyear)
+        print("maxyear = %d" % args.maxyear)
+        print("nsnap = %d" % args.nsnap)
+        print("dustSolver = %s" % args.dustSolver)
+        print("alpha = %f" % args.alpha)
+        print("amplitude = %f" % args.amplitude)
+        print("bumpVelFactor = %d" % args.bumpVelFactor)
+        print("timeBumpForm = %f" % args.timeBumpForm)
+        print("iniBumpPeakPos = %d" % args.iniBumpPeakPos)
+        print("MdiskInMstar = %f" % args.MdiskInMstar)
+        print("rmin = %d" % args.rmin)
+        print("rmax = %d" % args.rmax)
+        print("Nr = %d" % args.Nr)
+        print("Nm = %d" % args.Nm)
+        print("mmax = %d" % args.massMax)
+        print("starmass = %f" % args.starmass)
+        print("aIniMax = %f" % args.aIniMax)
+        print("dustEvolution = %d" % args.dustEvolution)
+        print("gasEvolution = %d" % args.gasEvolution)
+
+
+def setPlanForm(s):
+    """
+    Sets systole and diastole for planetesimal formation
+
+    Input:
+    ---------
+    s: Instance of the Simulation class
+    """
+    # The functions we created above use fields that do not exist in a DustPy a-priori
+    # We need to create the grids before the initialization
+    s.dust.dSigmaDust = np.zeros((s.grid.Nr, s.grid.Nm))
+    s.dust.SigmaPlan = np.ones(s.grid.Nr) * 1.e-100
+
+    # Calculate dust sources
+    s.dust.updater.systole = setPlanetesimalFormation(s)
+
+    # Apply them
+    s.dust.S.ext = dustSources(s)
+
+    # Update after timestep
+    s.dust.updater.diastole = addPlanetesimals(s)
+
+
+def setSimulationParams(s, args):
+    """
+    Sets output of simulation data and other simulation settings
+
+    Input:
+    ---------
+    s: Instance of the Simulation class
+    args: command line arguments
+    """
+
+    # Output settings
+    localDir = '/media/elle/Seagate Backup Plus Drive/2020/mpia/debris-discs'
+    slurmDir = '/media/elle/Seagate Backup Plus Drive/2020/mpia/mpia'
+    s.writer.overwrite = True
+    if path.exists(localDir):
+        s.writer.datadir = localDir + '/sims/' + str(args.outputDirNo)
+    elif path.exists(slurmDir):
+        s.writer.datadir = slurmDir + '/sims/' + str(args.outputDirNo)
+    else:
+        print("Output directory not found")
+        return
+
+    # Simulation settings
+    s.t.snapshots = np.linspace(args.minyear, args.maxyear, num=args.nsnap, endpoint=True) * c.year
+
+    if not args.gasEvolution:
+        del (s.integrator.instructions[1])
+
+    if not args.dustEvolution:
+        # Turn off all dust evolution
+        s.dust.S.tot = 0.
+        s.dust.S.updater = None
+        s.dust.S.coag = 0
+        s.dust.S.coag.updater = None
+        s.dust.p.updater = None
+        s.dust.v.rel.updater = None
+        s.dust.v.frag.updater = None
+        s.dust.kernel.updater = None
+        s.dust.p.stick = 1.
+        s.dust.p.frag = 0.
+        s.dust.p.updater = None
+        s.dust.Fi.adv = 0
+        s.dust.Fi.adv.updater = None
+        s.dust.v.rad = 0
+        s.dust.v.rad.updater = None
+        s.dust.Fi.diff = 0.
+        s.dust.Fi.diff.updater = None
+        s.dust.D.updater = None
+        s.dust.S.hyd = 0.
+        s.dust.S.hyd.updater = None
+        s.dust.Fi.updater = None
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-z', action="store", dest="outputDirNo", type=int, default=1, help="Simulation number")
+    parser.add_argument('-l', action="store", dest="rmin", type=int, default=10, help="Inner radius limit (AU)")
+    parser.add_argument('-c', action="store", dest="rmax", type=int, default=400, help="Outer radius limit (AU)")
+    parser.add_argument('-s', action="store", dest="minyear", type=float, default=0, help="Beginning year")
+    parser.add_argument('-e', action="store", dest="maxyear", type=float, default=10e6, help="Ending year")
+    parser.add_argument('-n', action="store", dest="nsnap", type=int, default=100, help="Number of snapshots")
+    parser.add_argument('-r', action="store", dest="Nr", type=int, default=300, help="Number of radial bins")
+    parser.add_argument('-m', action="store", dest="Nm", type=int, default=120, help="Number of mass bins")
+    parser.add_argument('-o', action="store", dest="starmass", type=float, default=1, help="Star Mass in solar units")
+    parser.add_argument('-a', action="store", dest="alpha", type=float, default=0.001, help="Viscosity parameter")
+    parser.add_argument('-b', action="store", dest="amplitude", type=float, default=10, help="log(Amplitude)")
+    parser.add_argument('-w', action="store", dest="width", type=float, default=1., help="width factor")
+    parser.add_argument('-t', action="store", dest="timeBumpForm", type=float, default=0, help="Time bump appears")
+    parser.add_argument('-v', action="store", dest="bumpVelFactor", type=float, default=0, help="% of nominal")
+    parser.add_argument('-p', action="store", dest="iniBumpPeakPos", type=int, default=90, help="Starting center (AU)")
+    parser.add_argument('-i', action="store", dest="MdiskInMstar", type=float, default=0.1, help="Inital disk mass SM")
+    parser.add_argument('-g', action="store", dest="aIniMax", type=float, default=1e-4, help="Max initial dust size")
+    parser.add_argument('-x', action="store", dest="massMax", type=float, default=1e5, help="Max mass")
+    parser.add_argument('-1', action="store", dest="gasEvolution", type=int, default=1, help="Create bump via alpha")
+    parser.add_argument('-2', action="store", dest="dustEvolution", type=int, default=1, help="Advect/diffus transport")
+    parser.add_argument('-4', action="store", dest="dustSolver", type=str, default="EXPL_EULER", help="EXPL_EULER")
+    arguments = parser.parse_args()
+    main(arguments)
