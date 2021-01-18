e9d1c10363e06c57ff8a98856a62c2b9498f0541
Elle <elle.miller101@gmail.com>
2021-01-19 07:12:49 +1100

    running w/o PF

--- runBasic4.sh
+++ runBasic4.sh
@@ -1,18 +1,13 @@
 #!/bin/bash -l
-#SBATCH --job-name=basic4
+#SBATCH --job-name=testPlanOff
 #SBATCH --mail-user elle.miller101@gmail.com
 #SBATCH --mail-type=ALL
+#SBATCH -n 1
 
-# Allocate 4 GB of Ram per node
-# --mem-per-cpu can be used to allocate RAM per CPU
-#SBATCH --mem=4000
-
-# Run 4 tasks (copies) of this job, use this for parallel jobs
-# The default is one task per node, can be overwritten with --cpus-per-task
-#SBATCH -n 4
+export OMP_NUM_THREADS=4
 
 module load anaconda3
 module load openmpi-4.0.1
 source activate dustpy2
 
-srun python ./mainBasic.py -z 87
+srun python ./main.py -n 31 -a 1e-4 -b 30 -p 30 -r 300 -e 6 -4 0 -z 127
